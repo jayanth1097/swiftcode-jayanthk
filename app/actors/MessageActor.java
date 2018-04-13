992b9e803ed5bd36b6082401fc9a480850475fcc
jayanth1097 <jayanth.kothakota@gmail.com>
2018-04-13 10:26:05 +0000

    commit intial

---
+++ app/actors/MessageActor.java
@@ -0,0 +1,11 @@
+package actors;
+
+import akka.actor.UntypedActor;
+
+public class MessageActor extends UntypedActor {
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+
+    }
+}
