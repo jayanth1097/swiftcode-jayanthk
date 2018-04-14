95de9d36d0eaf524204728c33424dac61cc0ace3
jayanth <jayanth@gmail.com>
2018-04-14 12:12:47 +0000

    added front end

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,5 +2,30 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
 
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'Hi wat can i do for u'
+},
+        {
+            'sender': 'USER',
+            'text': 'just checking u out'
+},
+        {
+            'sender': 'BOT',
+            'text': 'sorry please i cant understand'
+},
+        {
+            'sender': 'USER',
+            'text': 'ok wats up'
+},
+        {
+            'sender': 'BOT',
+            'text': 'ask anything u want'
+}
+];
 });
